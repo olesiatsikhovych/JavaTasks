1339467c1ee06a2a01caa75328f6d06527f25ff7
Olesia Tsikhovych <olesia1tsikhovych20@gmail.com>
2023-12-11 13:26:45 +0200

    Add Task10

---
+++ Task10/src/main/java/model/Person.java
@@ -0,0 +1,12 @@
+package model;
+
+@Employee(company = "Boobochcka")
+public class Person {
+    String name;
+    String surname;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
